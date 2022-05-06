9f752ae9df52680c84bbce84b98560a66e322932
Lasse Aakjær <51378679+TheLoGgI@users.noreply.github.com>
2022-05-07 00:41:16 +0200

    improve: public snippets display

--- app/components/SnippetListItem.jsx
+++ app/components/SnippetListItem.jsx
@@ -1,26 +1,28 @@
-import { StarIcon, PublicIcon, PrivateIcon } from "~/components/Icons"
+import { PrivateIcon, PublicIcon, StarIcon } from "~/components/Icons"
 import { Link } from "remix"
 
 export default function SnippetListItem({ snippet, languageTag }) {
-    const dateAdded = new Date(snippet.dateAdded)
-    const displayDate = `${dateAdded.getDate()}-${dateAdded.getMonth()}-${dateAdded.getFullYear()}`
+    const dateAdded = new Date(snippet.dateAdded).toLocaleDateString("da-DK", {
+        dateStyle: "long",
+    })
 
     return (
         <li className="rounded-lg hover:bg-blue-600  bg-blue-800 dark:bg-gray-800 dark:hover:bg-gray-700 p-3 mb-1 min-w-200-px">
             <Link to={`/snippets/${languageTag}/${snippet._id}`}>
-                <h2 className=" text-white text-xl font-semibold">
-                    {snippet.title}
-                </h2>
+                <div className="flex justify-between text-gray-300">
+                    <h2 className=" text-white text-xl font-semibold">
+                        {snippet.title}
+                    </h2>
+                    {snippet.visibility && <PublicIcon />}
+                </div>
                 <p className=" text-white  dark:text-gray-400">
                     {snippet.description.substring(0, 30)} ...
                 </p>
                 <div className="flex justify-between mt-1">
                     <p className=" text-white dark:text-gray-300">
-                        {displayDate}
+                        {dateAdded}
                     </p>
-                    <div>
-                        {snippet.visibility ? <PublicIcon /> : <PrivateIcon />}
-                    </div>
+
                     <StarIcon fill={snippet.favorite} />
                 </div>
             </Link>
