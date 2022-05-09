e6530530af7bbe4ace34e173d8cd4cd8614aefa7
Jonathan-SS <56107465+Jonathan-SS@users.noreply.github.com>
2022-05-09 13:50:17 +0200

    basic sw fetch that works

--- public/sw.js
+++ public/sw.js
@@ -31,54 +31,54 @@ self.addEventListener("activate", async () => {
 // Give images from cache if available
 // Else fetch from network and cache
 
-self.addEventListener("fetch", async (fetchEvent) => {
-    // Return all request that is not assets for rendering the snippet application.
-    //  eg. POST, PUT, DELETE, PATCH, HEAD, OPTIONS, TRACE
-    // Like creating new snippets, or updating existing snippets
-    if (fetchEvent.request.method !== "GET") return
+// self.addEventListener("fetch", async (fetchEvent) => {
+//     // Return all request that is not assets for rendering the snippet application.
+//     //  eg. POST, PUT, DELETE, PATCH, HEAD, OPTIONS, TRACE
+//     // Like creating new snippets, or updating existing snippets
+//     if (fetchEvent.request.method !== "GET") return
 
-    // ignore chrome extension, e.g don't cache them
-    if (fetchEvent.request.url.includes("chrome-extension")) return
+//     // ignore chrome extension, e.g don't cache them
+//     if (fetchEvent.request.url.includes("chrome-extension")) return
 
-    // fetchEvent.waitUntil(async () => {
-    const fetchResponse = await fetch(fetchEvent.request)
+//     // fetchEvent.waitUntil(async () => {
+//     const fetchResponse = await fetch(fetchEvent.request)
 
-    if (!fetchResponse.ok) return // Return error if not ok
-    // If not ok, and has status code between 500 and 599, return error
-    // If status code is between 400 and 499, return response from cache
+//     if (!fetchResponse.ok) return // Return error if not ok
+//     // If not ok, and has status code between 500 and 599, return error
+//     // If status code is between 400 and 499, return response from cache
 
-    const type = fetchResponse.headers.get("content-type") // Alternativ use fetchEvent.request.destination, content-type more reliable
+//     const type = fetchResponse.headers.get("content-type") // Alternativ use fetchEvent.request.destination, content-type more reliable
 
-    // Use image in cache, after fetch and put to cache
-    if (type.includes("image")) {
-        fetchEvent.respondWith(
-            (async function () {
-                // Checks cache for requst data
-                const cachedResponse = await caches.match(fetchEvent.request)
+//     // Use image in cache, after fetch and put to cache
+//     if (type.includes("image")) {
+//         await fetchEvent.respondWith(cacheFirst(fetchEvent)) // stop function after responseWith is resolved
+//     }
+//     fetchEvent.respondWith(networkFirst(fetchEvent))
 
-                // If requst data exists, return the data as response
-                if (cachedResponse) return cachedResponse
+//     // // Cache the rest that has same origin e.g css, js, html etc
+//     // if (fetchEvent.request.host === location.host) {
+//     //     console.log("fetchEvent.request: ", fetchEvent.request)
+//     //     const cache = await caches.open(staticCache)
+//     //     cache.put(fetchEvent.request, fetchResponse.clone())
+//     // }
 
-                // Cache the network respose data
-                const cache = await caches.open(imageCache)
-                cache.put(fetchEvent.request, fetchResponse.clone())
-                // Alternative:  cache.add(fetchEvent.request) - add to cache, same as fetch + put
+//     return response
+// })
+self.addEventListener("fetch", async function (event) {
+    if (event.request.url.includes("chrome-extension")) return // ignore chrome extension, e.g don't cache them
+    if (event.request.method !== "GET") return
 
-                // Return the network data as response
-                return fetchResponse
-            })()
-        )
-        return // stop function after responseWith is resolved
-    }
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request)
+        })
+    )
 
-    // Cache the rest that has same origin e.g css, js, html etc
-    if (fetchEvent.request.host === location.host) {
-        console.log("fetchEvent.request: ", fetchEvent.request)
+    if (fetchResponse) {
         const cache = await caches.open(staticCache)
-        cache.put(fetchEvent.request, fetchResponse.clone())
+        cache.put(event.request, fetchResponse.clone())
+        console.log("caching")
     }
-
-    return fetchResponse
 })
 
 // The notificationclick event - https://developers.google.com/web/ilt/pwa/introduction-to-push-notifications
